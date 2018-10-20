22eb0c5086128bcd6c9ae545252a7ac4e786d36a
Polina Matavina <paulin.mipt@gmail.com>
2018-10-21 01:45:57 +0300

    add a multiple city input

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import json
 app = Flask(__name__)
 
 
-def bad_arguments():
-    return 'Sorry, bad city set... Go back and try again, please'
+def bad_arguments(cause):
+    return 'Sorry, bad city set... Go back and try again, please!\nPossible fault: {}.'.format(cause)
 
 
 @app.route('/cities', methods=('GET', 'POST'))
@@ -13,23 +13,25 @@ def cities():
     with open('static/city_suggest.json') as fin:
     	return jsonify(json.load(fin))
 
+
 @app.route('/', methods=('GET', 'POST'))
 def main():
     if request.method == 'POST':
         cities = []
-        if 'city1' in request.form:
-            cities.append(request.form['city1'])
-        if 'city2' in request.form:
-            cities.append(request.form['city2'])
+        for key, values in request.form.lists():
+            if key == 'city[]':
+                cities = list(values)
         return redirect(url_for('render_map', cities='+'.join(cities)))
     return render_template('index.html')
 
-@app.route('/<cities>')
+
+@app.route('/<path:cities>')
 def render_map(cities):
     cities = cities.split('+')
-    if len(cities) != 2:
-        return bad_arguments()
+    if len(cities) < 2:
+        return bad_arguments('are you travelling alone? You should add more than 1 city')
     return render_template('map.html')
+
 
 if __name__ == '__main__':
     app.run()
