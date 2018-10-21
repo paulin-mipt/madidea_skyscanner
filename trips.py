dec326c30ccefac23b1c29215cfc86944023312b
Polina Matavina <paulin.mipt@gmail.com>
2018-10-21 05:43:39 +0300

    unmerge colliding names

--- trips.py
+++ trips.py
@@ -9,11 +9,13 @@ def get_flights(cities):
                             'scraper', 'skyscanner.sqlite')
     engine = db.create_engine('sqlite:///' + db_path)
     conn = engine.connect()
+    cities = list(map(lambda x: x.lower(), cities))
+
     query = (
-        'select date, price, cityA.name, cityB.name, cityA.lat, cityA.lon, cityB.lat, cityB.lon from quotes as q ' +
+        'select date, price, cityA.name, cityA.id, cityB.name, cityB.id, cityA.lat, cityA.lon, cityB.lat, cityB.lon from quotes as q ' +
         'inner join city as cityA on cityA.id == q."city_A" ' +
         'inner join city as cityB on cityB.id == q."city_B" ' +
-        'where cityA.name in (\"{0}\")'.format(
+        'where cityA.id in (\"{0}\")'.format(
             '\", \"'.join(cities)
             )
     )
@@ -21,15 +23,18 @@ def get_flights(cities):
 
     min_price = float('+inf')
     best_date = None
-    best_city = None
+    best_city_id = None
 
     groups = defaultdict(lambda: defaultdict(dict))
     coordinates = {}
-    for date, price, aname, bname, alat, alon, blat, blon in data:
+    names = {}
+    for date, price, aname, aid, bname, bid, alat, alon, blat, blon in data:
         date = date.split()[0]  # take only date part of datetime string
-        groups[date][bname][aname] = float(price)
-        coordinates[bname] = (blat, blon)
-        coordinates[aname] = (alat, alon)
+        groups[date][bid][aid] = float(price)
+        coordinates[bid] = (blat, blon)
+        coordinates[aid] = (alat, alon)
+        names[bid] = bname
+        names[aid] = aname
 
     for date in groups:
         for destination in groups[date]:
@@ -38,10 +43,9 @@ def get_flights(cities):
                 if price < min_price:
                     min_price = price
                     best_date = date
-                    best_city = destination
+                    best_city_id = destination
 
-
-    if best_date is None or best_city is None:
+    if best_date is None or best_city_id is None:
         return None, None, []
 
     result = []
@@ -49,9 +53,9 @@ def get_flights(cities):
         result.append({
             'a_lat' : coordinates[city][0],
             'a_lon' : coordinates[city][1],
-            'b_lat' : coordinates[best_city][0],
-            'b_lon' : coordinates[best_city][1],
-            'cost' : groups[best_date][best_city][city]
+            'b_lat' : coordinates[best_city_id][0],
+            'b_lon' : coordinates[best_city_id][1],
+            'cost' : groups[best_date][best_city_id][city]
         })
 
-    return best_date, best_city, result
+    return best_date, names.get(best_city_id), result
