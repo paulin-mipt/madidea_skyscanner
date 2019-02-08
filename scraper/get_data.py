15bf49c61a895396b19573242d739f0ec19dc0a4
Aleksei Kulikov <alexeyqu@yandex-team.ru>
2019-02-09 02:12:38 +0300

    1st attempt to work with aviasales api

--- scraper/get_data.py
+++ scraper/get_data.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 import json
 import datetime 
 import tqdm
+import sys
 
 
 n_days = 30
@@ -14,34 +15,18 @@ today = datetime.datetime.today()
 
 Base = declarative_base()
 
-class Country(Base):
-    __tablename__ = 'country'
-    id = Column(String, primary_key=True)
-    name = Column(String)
-    currency = Column(String)
-
-    def __init__(self, id, name, currency):
-        self.id = id
-        self.name = name
-        self.currency = currency
-
-    def __repr__(self):
-        return "<Country('%s','%s', '%s')>" % (self.id, self.name, self.currency)
-
 
 class City(Base):
     __tablename__ = 'city'
     id = Column(String, primary_key=True)
     name = Column(String)
-    code = Column(String)
     country_id = Column(String)
     lat = Column(Float)
     lon = Column(Float)
 
-    def __init__(self, id, name, code, country_id, lat, lon):
+    def __init__(self, id, name, country_id, lat, lon):
         self.id = id
         self.name = name
-        self.code = code
         self.country_id = country_id
         self.lat = lat
         self.lon = lon
@@ -50,8 +35,8 @@ class City(Base):
         return "<City('%s','%s', '%s')>" % (self.name, self.lat, self.lon)
 
 
-class Quote(Base):
-    __tablename__ = 'quotes'
+class Route(Base):
+    __tablename__ = 'routes'
     id = Column(Integer, primary_key=True, autoincrement=True)
     city_A = Column(String)
     city_B = Column(String)
@@ -67,74 +52,61 @@ class Quote(Base):
         self.url = url
 
     def __repr__(self):
-        return "<Quote('%s','%s', '%s', '%s')>" % (self.id, self.city_A, self.city_B, self.date)
+        return "<Route('%s','%s', '%s', '%s')>" % (self.id, self.city_A, self.city_B, self.date)
 
 
-def get_cities_data(apiKey):
-    resp = requests.get('http://partners.api.skyscanner.net/apiservices/geo/v1.0',
-                    params={'apiKey': apiKey})
-    return resp.json()
+def fill_geo_from_file(path, session):
+    with open(path) as json_data:
+        data = json.load(json_data)
+    # {"time_zone":"America/Denver","name":"Santa Fe","coordinates":{"lon":120.43236,"lat":30.236935},"code":"ZSH","country_code":"US"}
+    for city in data:
+        try:
+            obj = City(
+                city['code'],
+                city['name'],
+                city['country_code'],
+                city['coordinates']['lat'],
+                city['coordinates']['lon'])
+            session.add(obj)
+        except Exception as e:
+            print(city, e)
+    session.commit()
 
 
-def fill_geo(apiKey, session):
-    data = get_cities_data(apiKey)
-    for cont in data['Continents']:
-        print(cont['Name'], cont['Id'])
-        for country in cont['Countries']:
-            print(country['Name'])
-            cntry = Country(country['Id'], country['Name'], country['CurrencyId'])
-            session.add(cntry)
-            for city in country['Cities']:
-                loc = city['Location']
-                lat = float(loc.split(',')[1].strip())
-                lon = float(loc.split(',')[0].strip())
-                cty = City(city['Id'], city['Name'], city['IataCode'], city['CountryId'], lat, lon)
-                session.add(cty)
-            session.commit()
-
-
-def get_quotes(A, B, date, apiKey, locale='en-GG', country='ES', currency='EUR'):
-    req = ['http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0',
-           country,
-           currency,
-           locale,
-           A, B, date
-    ]
-    req = '/'.join(req)
-    print(req)
-    resp = requests.get(req,
-                       params={'apiKey': apiKey})
+def get_routes(A, B, date, apiKey, currency='EUR'):
+    resp = requests.get('http://api.travelpayouts.com/v2/prices/month-matrix', params={
+        'month': date,
+        'origin': A,
+        'destination': B,
+        # 'currency': currency,
+        'token': apiKey
+    })#, config={'verbose': sys.stderr})
     return resp
 
-
-def process_quotes(data):
-    places = {}
-    for place in data['Places']:
-        if 'CityId' in place:
-            places[place['PlaceId']] = {'CityId': place['CityId'].lower()}
-    for quote in data['Quotes']:
-        yield (
-            places[quote['OutboundLeg']['OriginId']]['CityId'],
-            places[quote['OutboundLeg']['DestinationId']]['CityId'],
-            quote['MinPrice']
-        )
+# {'number_of_changes': 0, 'found_at': '2019-02-08T18:29:55', 'distance': 1938, 'duration': 170, 'actual': True, 
+#  'value': 8748.0, 'gate': 'OneTwoTrip', 'show_to_affiliates': True, 'origin': 'MOW', 'return_date': None, 
+#  'trip_class': 0, 'depart_date': '2019-02-10', 'destination': 'SLY'}
+def parse_routes(data):
+    for route in data:
+        yield route['origin'], route['destination'], route['depart_date'], route['value']
 
 
-def fill_quotes_graph(apiKey, session):
+def fill_routes(apiKey, session):
+    always_present_city = 'MOW'
     all_cities = session.query(City).all()
-    for date in [today + datetime.timedelta(days=x) for x in range(1, n_days)]:
-        print(date.strftime("%Y%m%d"))
-        for cityA in tqdm.tqdm(all_cities):
-            quotes = get_quotes(cityA.id, 'anywhere', date.strftime("%Y-%m-%d"), 'ha812346737381611819258748469243')
-            data = quotes.json()
-            if 'Quotes' in data and len(data['Quotes']) == 0:
-                continue
-            else:
-                print(cityA)
-            for a, b, price in process_quotes(data):
-                quote = Quote(a, b, date, price)
-                session.add(quote)
-            session.commit()     
+    valid_cities = set()
+    for city in tqdm.tqdm(all_cities):
+        routes = get_routes(always_present_city, city.id, '2019-02-01', apiKey)
+        if routes.status_code == 200:
+            print(city.id)
+            valid_cities.add(city)
+            data = routes.json()['data']
+            if len(data) > 0:
+                for a, b, date, price in parse_routes(data):
+                    route = Route(a, b, datetime.datetime.strptime(date, '%Y-%m-%d'), price)
+                    session.add(route)
+                session.commit()     
+    print(len(valid_cities))
 
 
 def fill_data(path, apiKey):
@@ -142,11 +114,11 @@ def fill_data(path, apiKey):
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    fill_geo(apiKey, session)
-    fill_quotes_graph(apiKey, session)
+    fill_geo_from_file('./static/cities.json', session)
+    fill_routes(apiKey, session)
 
 
 if __name__ == '__main__':
     with open('apiKey.cred', 'r') as cred:
         apiKey = cred.read()
-    fill_data('skyscanner1.sqlite', apiKey)
+    fill_data('aviasales1.sqlite', apiKey)
